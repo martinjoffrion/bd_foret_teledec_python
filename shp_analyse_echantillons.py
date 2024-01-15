fcd1f864edff0b88d2476ed2d1bd3d98f6cf2ff2
clairearondel <133198989+clairearondel@users.noreply.github.com>
2024-01-15 22:01:40 +0100

    Update shp_analyse_echantillons.py
    1729 rows

--- shp_analyse_echantillons.py
+++ shp_analyse_echantillons.py
@@ -8,42 +8,40 @@ Created on Wed Dec  6 17:19:39 2023
 import pandas as pd
 import geopandas as gpd
 #conda install openpyxl
-import os
-os.environ['MYOTB'] = 'D:/OTB-8.1.2-Win64/bin' #path to the OTB bin folder
 working_directory = 'D:/projet_teledetection_python'
 
+nomenclature = pd.read_excel('D:/projet_teledetection_python/nomenclature.xlsx')#20 lignes et 8 colonnes
+emprise_etude = 'D:/projet_teledetection_python/data_set/emprise_etude.shp'
+bd_foret='D:/projet_teledetection_python/data_set/FORMATION_VEGETALE.shp'
 
-
-nomenclature = pd.read_excel('D:/projet_teledetection_python/nomenclature.xlsx')
-emprise_etude = 'D:/projet_teledetection_python/emprise_etude.shp'
-bd_foret='D:/projet_teledetection_python/BDFORET_2-0__SHP_LAMB93_D031_2019-01-09/BDFORET/1_DONNEES_LIVRAISON/BDF_2-0_SHP_LAMB93_D031/FORMATION_VEGETALE.shp'
-
-folder_traitement = 'D:/projet_teledetection_python/bd_foret'
+#Lecture des fichiers
 foret = gpd.read_file(bd_foret)
 emprise = gpd.read_file(emprise_etude)
-    # Identifier les valeurs à supprimer
-valeurs_a_supprimer = ['Lande', 'Formation herbacée'] #données a supprimer dans la bd foret
+# Identifier les valeurs à supprimer
+valeurs_a_supprimer = ['Lande', 'Formation herbacée',
+                       'Forêt ouverte de feuillus purs',
+                       'Forêt ouverte de conifères purs',
+                       'Forêt ouverte sans couvert arboré',
+                       'Forêt ouverte à mélange de feuillus et conifères'] #données a supprimer dans la bd foret
 foret_filtre = foret[~foret['TFV'].isin(valeurs_a_supprimer)] #supprime les valeurs de la bd foret non souhaité
-intersection = gpd.overlay(foret_filtre, emprise, how='intersection')
-intersection.to_file(folder_traitement, driver="ESRI Shapefile")
+
+#emprise des images S2 que vous allez utiliser
+bd_foret_ok = gpd.clip(foret_filtre, emprise)
 
 
-
-
-##lire le shp
-bd_foret = gpd.read_file('D:/projet_teledetection_python/bd_foret.shp')
-bd_foret_nomenclature = pd.merge(bd_foret, nomenclature, on='CODE_TFV', how='inner')
-supp_col = ['raster', 'MINX', 'MINY', 'MAXX', 'MAXY', 'CNTX', 'CNTY', 'AREA', 'PERIM', 'HEIGHT', 'WIDTH', 'MINX_2', 'MINY_2', 'MAXX_2', 'MAXY_2', 'CNTX_2', 'CNTY_2', 'AREA_2', 'PERIM_2', 'HEIGHT_2', 'WIDTH_2','TFV_y']
-bd_foret_nomenclature = bd_foret_nomenclature.drop(supp_col, axis=1)
+bd_foret_nomenclature = pd.merge(bd_foret_ok, nomenclature, on='CODE_TFV', how='inner')
+#nettoyage
+bd_foret_nomenclature = bd_foret_nomenclature.drop(columns=['TFV_y'])
 bd_foret_nomenclature = bd_foret_nomenclature.rename(columns={'TFV_x' : 'TFV'})
 
-
-bd_foret_nomenclature.to_file(f'{working_directory}/bd_foret_nomenclature.shp', driver="ESRI Shapefile")
-print(bd_foret_nomenclature.TFV_x == 'Forêt fermée de feuillus purs en îlots')
-
-###condition 
+#si une classe TFV contient moins de 15 polygones dans la classe 3, alors ne la prenez pas en compte.
 seuil_min_polygones = 15
-comptage_polygones = bd_foret_nomenclature.groupby('CODE_TFV')['Code_lvl3'].count()
+comptage_polygones = bd_foret_nomenclature.groupby('Code_lvl3')['Code_lvl3'].count()
+
 classes_a_supprimer = comptage_polygones[comptage_polygones <= seuil_min_polygones].index
 df_filtre = bd_foret_nomenclature[~bd_foret_nomenclature['Code_lvl3'].isin(classes_a_supprimer)]
 print(df_filtre)
+
+#test
+folder_traitement = 'D:/projet_teledetection_python/Sample_BD_foret_T31TCJ'
+df_filtre.to_file(folder_traitement, driver="ESRI Shapefile")
