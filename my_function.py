456ec367ba2d3d78c067730bbd0b433f9eac132b
clairearondel <133198989+clairearondel@users.noreply.github.com>
2024-01-24 20:42:24 +0100

    Update my_function.py
    Rajout de get_sample

--- my_function.py
+++ my_function.py
@@ -9,9 +9,13 @@ import os
 import rasterio
 from rasterio.mask import mask
 import geopandas as gpd
+import numpy as np
+import pandas as pd
 from osgeo import gdal
 import matplotlib as plt
+#testcommentaire
 otb_bin_path = os.environ['MYOTB']
+##fonction
 RAM = os.environ['MYRAM']
 
 def traitement_forest (bd_foret_path, out_dir):
@@ -336,7 +340,119 @@ def cmd_Rasterization(sample_bdforet_filename, out_sample_filename, image_filena
     os.system(cmd)
 
 
-### Fonctions du cours
+### ----- Fonctions du cours
+def get_samples_from_roi(raster_name, roi_name, value_to_extract=None,
+                         bands=None, output_fmt='full_matrix'):
+    '''
+    The function get the set of pixel of an image according to an roi file
+    (raster). In case of raster format, both map should be of same
+    size.
+
+    Parameters
+    ----------
+    raster_name : string
+        The name of the raster file, could be any file GDAL can open
+    roi_name : string
+        The path of the roi image.
+    value_to_extract : float, optional, defaults to None
+        If specified, the pixels extracted will be only those which are equal
+        this value. By, defaults all the pixels different from zero are
+        extracted.
+    bands : list of integer, optional, defaults to None
+        The bands of the raster_name file whose value should be extracted.
+        Indexation starts at 0. By defaults, all the bands will be extracted.
+    output_fmt : {`full_matrix`, `by_label` }, (optional)
+        By default, the function returns a matrix with all pixels present in the
+        ``roi_name`` dataset. With option `by_label`, a dictionnary
+        containing as many array as labels present in the ``roi_name`` data
+        set, i.e. the pixels are grouped in matrices corresponding to one label,
+        the keys of the dictionnary corresponding to the labels. The coordinates
+        ``t`` will also be in dictionnary format.
+
+    Returns
+    -------
+    X : ndarray or dict of ndarra
+        The sample matrix. A nXd matrix, where n is the number of referenced
+        pixels and d is the number of variables. Each line of the matrix is a
+        pixel.
+    Y : ndarray
+        the label of the pixel
+    t : tuple or dict of tuple
+        tuple of the coordinates in the original image of the pixels
+        extracted. Allow to rebuild the image from `X` or `Y`
+    '''
+
+    # Get size of output array
+    raster = gdal.Open(raster_name, gdal.GA_ReadOnly)
+    nb_col, nb_row, nb_band = raster.RasterXSize, raster.RasterYSize, raster.RasterCount
+    # Get data type
+    band = raster.GetRasterBand(1)
+    gdal_data_type = gdal.GetDataTypeName(band.DataType)
+    
+    if gdal_data_type == 'Byte':
+        numpy_data_type = 'uint8'
+    else:
+        numpy_data_type = gdal_data_type.lower()
+
+    # Check if is roi is raster or vector dataset
+    roi = gdal.Open(roi_name, gdal.GA_ReadOnly)
+
+    if (raster.RasterXSize != roi.RasterXSize) or \
+            (raster.RasterYSize != roi.RasterYSize):
+        print('Images should be of the same size')
+        print('Raster : {}'.format(raster_name))
+        print('Roi : {}'.format(roi_name))
+        exit()
+
+    if not bands:
+        bands = list(range(nb_band))
+    else:
+        nb_band = len(bands)
+
+    #  Initialize the output
+    ROI = roi.GetRasterBand(1).ReadAsArray()
+    if value_to_extract:
+        t = np.where(ROI == value_to_extract)
+    else:
+        t = np.nonzero(ROI)  # coord of where the samples are different than 0
+
+    Y = ROI[t].reshape((t[0].shape[0], 1)).astype('int32')
+
+    del ROI
+    roi = None  # Close the roi file
+
+    try:
+        X = np.empty((t[0].shape[0], nb_band), dtype=numpy_data_type)
+    except MemoryError:
+        print('Impossible to allocate memory: roi too large')
+        exit()
+
+    # Load the data
+    for i in bands:
+        temp = raster.GetRasterBand(i + 1).ReadAsArray()
+        X[:, i] = temp[t]
+    del temp
+    raster = None  # Close the raster file
+
+    # Store data in a dictionnaries if indicated
+    if output_fmt == 'by_label':
+        labels = np.unique(Y)
+        dict_X = {}
+        dict_t = {}
+        for lab in labels:
+            coord = np.where(Y == lab)[0]
+            dict_X[lab] = X[coord]
+            dict_t[lab] = (t[0][coord], t[1][coord])
+
+        return dict_X, Y, dict_t
+    else:
+        return X, Y, t,
+    
+    
+
+
+
+
 def report_from_dict_to_df(dict_report):
     # convert report into dataframe
     report_df = pd.DataFrame.from_dict(dict_report)
