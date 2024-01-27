a6f4d0fa7ee5af71a029d6c07302c1320eb32435
clairearondel <133198989+clairearondel@users.noreply.github.com>
2024-01-27 20:48:47 +0100

    os.path.join

--- pre_traitement.py
+++ pre_traitement.py
@@ -23,6 +23,7 @@ os.environ['MYOTB'] = 'le/chemin/vers/OTB-8.1.2-Win64/bin'
 
 os.environ['MYRAM'] = '8000'
 import my_function as f
+import shutil
 
 # --- Chemin d'accès du dossier qui comprendra les résultats intermédiaires et les images concaténées par date :
 working_directory = 'le/chemin/vers/projet_teledection_sigmaM2_group4/traitement'
@@ -45,20 +46,20 @@ roi = 'le/chemin/vers/projet_teledection_sigmaM2_group4/data_set/emprise_etude.s
 os.chdir(working_directory)
 
 # --- Création d'un nouveau sous-dossier pour y stocker les résultats intermédiaires
-os.mkdir(f'{working_directory}/intermediate_result')
+os.mkdir(os.path.join(working_directory,'intermediate_result'))
 # enregistre le chemin complet sous une variable
-iwdir = f'{working_directory}/intermediate_result'
+iwdir = os.path.join(working_directory, 'intermediate_result')
 
 # --- Création d'un sous-dossier pour y stocker les images concaténées par date
-os.mkdir(f'{working_directory}/date_result')
+os.mkdir(os.path.join(working_directory,'date_result'))
 # enregistre le chemin complet sous une variable
-ifwdir = f'{working_directory}/date_result'
+ifwdir = os.path.join(working_directory,'date_result')
 
 ########### --- Découpage et Rasterisation de la BD_FORET
 
 # nettoyage de la donnée et création d'une variable pour le champ raster
 gdf_mask, foret_filtre_path = f.traitement_forest(bd_foret, iwdir)
-foret_filtre_path = f'{iwdir}/{foret_filtre_path}'
+foret_filtre_path = os.path.join(iwdir, foret_filtre_path)
 out_image = 'masque_foret.tif'
 field = 'raster' # champ necessaire à la rasterisation
 # appel de la fonction rasterize_shapefile
@@ -68,9 +69,9 @@ f.rasterize_shapefile(foret_filtre_path, out_image, roi, field, 10)
 
 # boucle sur le nombre de sous-dossiers dans le dossier data_set (data_path)
 files = [os.path.join(data_path,f) for f in os.listdir(data_path)
-         if os.path.isdir(os.path.join(data_path,f))]
-result_1 = glob.glob(F"{files[0]}/*FRE_B2.tif")
-img_path = result_1[0]
+         if os.path.isdir(os.path.join(data_path, f))]
+result_1 = os.path.join(files[0], "*FRE_B2.tif")
+img_path = glob.glob(result_1)[0]
 
 img = rasterio.open(img_path)
 emprise = gpd.read_file(roi)
@@ -80,8 +81,8 @@ img_crs = img.crs
 emprise_32631 = emprise.to_crs(img_crs)
 
 # sauvegarde de la nouvelle projection de l'emprise
-emprise_32631.to_file(f'{iwdir}/emprise_32631', driver = 'ESRI Shapefile')
-new_emprise = f'{iwdir}/emprise_32631'
+emprise_32631.to_file(os.path.join(iwdir, 'emprise_32631'), driver = 'ESRI Shapefile')
+new_emprise = os.path.join(iwdir, 'emprise_32631')
 
 ########### --- Début de la phase de pré-traitement
 
@@ -91,13 +92,14 @@ list_date_path = []
 list_ndvi_path = []
 
 for subfil in range(len(files)):
-    
+         
     # liste des bandes recherchées
     list_band = ['B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B11', 'B12']
     # créer une liste vide pour le nom de futurs fichiers par bande
     list_band_name = []
     for i in range (len(list_band)):
-        result = glob.glob(F"{files[subfil]}/*FRE_{list_band[i]}.tif")
+        result = os.path.join(files[subfil], f"*FRE_{list_band[i]}.tif")
+        result = glob.glob(result)
         if result == [] : result.append(' ')
         band = result[0]
         list_band_name.append(band)
@@ -113,9 +115,10 @@ for subfil in range(len(files)):
     for i in range (len(list_band_name)):
         input_img = list_band_name[i]
         band_name = os.path.basename(input_img)
-        output_raster = f'{iwdir}/cut{band_name}'
+        name = 'cut{band_name}'
+        output_raster = os.path.join(iwdir, name.format(band_name=band_name))
         # appel de la fonction cmd_ExtractROI
-        f.cmd_ExtractROI(input_img, new_emprise, output_raster)
+        f.cmd_ExtractROI(input_img,new_emprise,output_raster)
         list_bande_cut.append(output_raster)
         
     # convertir les "lists" en "dictionary"
@@ -136,7 +139,8 @@ for subfil in range(len(files)):
         inr = list_band_10m[0]
         inm = list_bande_20m_plus[i]
         band_name = os.path.basename(inm)
-        output_raster = f'{iwdir}/_10m_{band_name}'
+        name = '_10m_{band_name}'
+        output_raster = os.path.join(iwdir, name.format(band_name=band_name))
         # appel de la fonction cmd_Superimpose
         f.cmd_Superimpose(inr, inm, output_raster)
         list_bande_finale.append(output_raster)
@@ -146,34 +150,35 @@ for subfil in range(len(files)):
 ########### --- Concaténation 
 
     # appel de la fonction get_date_f_b_path
-    date = f.get_date_f_b_path(list_bande_finale[0]) # recupération la date de la première bande
-    output_concat = f'{ifwdir}/output_{date}_date.tif'
+    date =  f.get_date_f_b_path(list_bande_finale[0]) # recupération la date de la première bande
+    name = 'output_{date}_date.tif'
+    output_concat = os.path.join(iwdir, name.format(date=date))
     # appel de la fonction cmd_ConcatenateImages
     f.cmd_ConcatenateImages(list_bande_finale, output_concat)
     list_date_path.append(output_concat)
     
 ########### --- NDVI
 
-    # appel de la fonction rasterio_ndvi (la B2 correspond à la B4 (rouge) et la B3 à la B5 (infrarouge) 
+    # appel de la fonction rasterio_ndvi (la B2 correspond à la B4 (rouge) et la B3 à la B5 (infrarouge)) 
     ndvi_path = f.rasterio_ndvi(output_concat, 2, 3)
     list_ndvi_path.append(ndvi_path)
 
 ########### --- Concaténation finale des 6 dates
 
 output_concat = 'Serie_temp_S2_allbands.tif'
-f.cmd_ConcatenateImages(list_date_path, f'{iwdir}/{output_concat}', 'uint16')
-f.warp(f'{iwdir}/{output_concat}', f'{ifwdir}/{output_concat}', '2154')
-f.apply_mask( f'{ifwdir}/{output_concat}', output_concat, gdf_mask)
+f.cmd_ConcatenateImages(list_date_path, os.path.join(iwdir, output_concat), 'uint16')
+f.warp(os.path.join(iwdir, output_concat), os.path.join(ifwdir, output_concat), '2154')
+f.apply_mask(os.path.join(ifwdir, output_concat), output_concat, gdf_mask)
 
 ########### --- Concaténation finale des NDVI
 
 output_concat = 'Serie_temp_S2_ndvi.tif'
-f.cmd_ConcatenateImages(list_ndvi_path, f'{iwdir}/{output_concat}', 'float')
-f.warp(f'{iwdir}/{output_concat}', f'{ifwdir}/{output_concat}', '2154')
-f.apply_mask( f'{ifwdir}/{output_concat}', output_concat, gdf_mask)
+f.cmd_ConcatenateImages(list_ndvi_path, os.path.join(iwdir, output_concat), 'float')
+f.warp(os.path.join(iwdir, output_concat), os.path.join(ifwdir, output_concat), '2154')
+f.apply_mask(os.path.join(ifwdir, output_concat), output_concat, gdf_mask)
 
 ########### --- Suppression des résultats intermédiaires
 
-import shutil
 shutil.rmtree(iwdir)
+shutil.rmtree(ifwdir)
 
